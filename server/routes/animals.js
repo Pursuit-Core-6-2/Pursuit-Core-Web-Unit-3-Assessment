87ddf2b4118e97424e8d9dbd75779177c97d45f6
KathyPuma <kathypuma96@gmail.com>
2019-11-17 14:27:21 -0500

    fixed servers

--- server/routes/animals.js
+++ server/routes/animals.js
@@ -29,12 +29,16 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res) => {
     try {
         let selectQuery = `SELECT * FROM animals WHERE id = $1`
-        let singleResearcher = await db.any(selectQuery, parseInt(req.params.id))
-        res.json({
-            status: "success",
-            message: `Here is a single animal!`,
-            payload: singleResearcher
-        });
+        let singleAnimal = await db.any(selectQuery, parseInt(req.params.id))
+        if (singleAnimal.length === 1) {
+            res.json({
+                status: "success",
+                message: `Here is a single animal!`,
+                payload: singleAnimal
+            });
+        } else {
+            return error;
+        }
     } catch (error) {
         res.json({
             status: "error",
@@ -44,23 +48,48 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+router.post("/", async (req, res) => {
+    try {
+        let insertQuery = `INSERT INTO animals(species_id,nickname) 
+        VALUES($1,$2);`
+        await db.none(insertQuery, [req.body.species_id, req.body.nickname])
+        if (req.body.nickname === "") {
+            return error
+        } else {
+            res.json({
+                status: "success",
+                message: `Added a new animal`,
+                payload: req.body
+            });
+        }
+    } catch (error) {
+        res.json({
+            status: "error",
+            message: `no animal added!`
 
+        });
+    }
+})
 
 
 router.patch('/:id', async (req, res, next) => {
     try {
         let animal_id = parseInt(req.params.id)
-        let updateQuery = 
-        `UPDATE animals
+        let updateQuery =
+            `UPDATE animals
         SET species_id = $1, 
         nickname = $2
         wHERE id = $3`
-        await db.none(updateQuery, [req.body.species_id, req.body.nickname,animal_id])
-        res.json({
-            status: "success",
-            message: `Updated animal Info`,
-            payload: req.body
-        });
+        await db.none(updateQuery, [req.body.species_id, req.body.nickname, animal_id])
+        if (req.body.nickname === "") {
+            return error
+        } else {
+            res.json({
+                status: "success",
+                message: `Updated animal Info`,
+                payload: req.body
+            });
+        }
     } catch (error) {
         res.json({
             status: "error",
@@ -74,13 +103,22 @@ router.patch('/:id', async (req, res, next) => {
 
 router.delete("/:id", async (req, res) => {
     try {
-        let deleteQuery = `DELETE FROM researchers WHERE id = $1;`
-        await db.none(deleteQuery, parseInt(req.params.id));
-        res.json({
-            status: "success",
-            message: `Removed a researcher`,
-            payload: req.body
-        });
+
+        let selectQuery = `SELECT * FROM animals WHERE id = $1`
+        let singleAnimal = await db.any(selectQuery, parseInt(req.params.id))
+        console.log(singleAnimal.length)
+        if (singleAnimal.length === 1) {
+            let deleteQuery = `DELETE FROM animals WHERE id = $1;`
+            await db.none(deleteQuery, parseInt(req.params.id));
+
+            res.json({
+                status: "success",
+                message: `Removed a researcher`,
+                payload: singleAnimal
+            });
+        } else {
+            return error;
+        }
     } catch (error) {
         res.json({
             status: "error",
