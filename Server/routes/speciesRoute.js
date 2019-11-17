d414ca43db7ca89162199fe3faa70604edf5a052
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-11-17 13:24:48 -0500

    finished species by ID and I lied no lunch for me

--- Server/routes/speciesRoute.js
+++ Server/routes/speciesRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //pg-promise
 const db = require('./promiseConfig')
 
-//get all researchers
+//get all species
 router.get('/', async (req, res) => {
     console.log('Species endpoint reached/ ', Date())
     try {
@@ -21,6 +21,33 @@ router.get('/', async (req, res) => {
         })
     } catch (error) {
         res.status(500)
+        res.json({
+            status: "error",
+            message: "species not found",
+            payload: null
+        })
+    }
+})
+
+//Get species by ID
+router.get('/:species_id', async (req, res) => {
+    let findStaff = `
+        SELECT *
+        FROM species
+        WHERE id = $1
+    `
+    console.log('Species single endpoint reached/ ', Date())
+
+    try {
+        let species = await db.one(findStaff, [req.params.species_id])
+        res.json({
+            status: "success",
+            message: "retrieved single species",
+            payload: species
+        })
+    } catch (error) {
+        res.status(500)
+        console.log(error)
         res.json({
             status: "error",
             message: "species not found",
