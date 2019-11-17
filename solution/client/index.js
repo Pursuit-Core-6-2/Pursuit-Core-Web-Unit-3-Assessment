9dcebef5aa7b7c9dab599a37398e90cb33e32614
gsanchez13 <gisellesanchez@pursuit.org>
2019-11-17 16:02:25 -0500

    createOptions and getSightings functions created

--- solution/client/index.js
+++ solution/client/index.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hi');
+    const selectBox = document.querySelector('#selectTag');
+    createOptions(selectBox);
+    selectBox.addEventListener('change', getSightings);
 })
-const viewSightings = async () => {
-    console.log('Here is a sighting')
+const getSightings = async () => {
+    const selectBox = document.querySelector('#selectTag');
+    let selectBoxValue = Number(selectBox.value);
+    let url = `http://localhost:3000/sightings/researchers/${selectBoxValue}`;
+    let researcherSightings = await axios.get(url).then(response => {return response.data.payload})
+    console.log(researcherSightings)
 };
+const createOptions = async (selectBox) => {
+    let url = `http://localhost:3000/researchers`
+    let researcherObjs = await axios.get(url).then(response => {return response.data.payload});
+    researcherObjs.forEach((elem) => {
+        let newOption = document.createElement('option');
+        let researcherName = elem.r_name;
+        let researcherId = elem.id;
+            newOption.label = researcherName;
+            newOption.value = researcherId;
+            selectBox.append(newOption);
+    });
+}
