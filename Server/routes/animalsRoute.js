7cb6f9c47a88e8e43a8e716b46d9520f56ea6b1a
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-11-17 13:44:35 -0500

    can find animals by their ID

--- Server/routes/animalsRoute.js
+++ Server/routes/animalsRoute.js
@@ -29,5 +29,31 @@ router.get('/', async (req, res) => {
     }
 })
 
+//Get animal by ID
+router.get('/:animal_id', async (req, res) => {
+    let findStaff = `
+        SELECT *
+        FROM animals
+        WHERE id = $1
+    `
+    console.log('Species single endpoint reached/ ', Date())
+
+    try {
+        let animal = await db.one(findStaff, [req.params.animal_id])
+        res.json({
+            status: "success",
+            message: "retrieved single animal",
+            payload: animal
+        })
+    } catch (error) {
+        res.status(500)
+        // console.log(error)
+        res.json({
+            status: "error",
+            message: "Animal not found",
+            payload: null
+        })
+    }
+})
 
 module.exports = router;
