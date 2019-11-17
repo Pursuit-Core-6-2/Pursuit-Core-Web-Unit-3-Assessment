53f5a0c27ba923411905b0e61f754c96fa781d10
gsanchez13 <gisellesanchez@pursuit.org>
2019-11-17 14:53:35 -0500

    get sightings my species corrected

--- solution/server/routes/sightings.js
+++ solution/server/routes/sightings.js
@@ -19,22 +19,23 @@ router.get('/', async (req, res) => {
         });
     }
 });
-router.get('/:id', async (req, res) => {
-    let id = req.params.id;
+router.get('/:species_id', async (req, res) => {
+    let speciesId = req.params.species_id;
     try {
-        let sighting = await db.one('SELECT * FROM sightings WHERE id = $1', id);
+        let sightings = await db.any('SELECT * FROM sightings WHERE species_id = $1', speciesId);
         res.json({
             status: 'Success',
-            message: 'Sighting retrieved',
-            payload: sighting
+            message: 'Sightings by species retrieved',
+            payload: sightings
         });
     }
     catch(error) {
         res.json({
             status: 'Error',
-            message: 'Could not load sighting.',
+            message: 'Could not load sighting by species.',
             payload: null,
         });
     }
 });
+
 module.exports = router;
